c973a190d99a6a20272d7661a0779461c3b9b060
EstevenJaviier <Estevenjaviier@gmail.com>
2023-01-30 16:25:31 -0500

    spa installed

---
+++ src/root.component.tsx
@@ -0,0 +1,14 @@
+const Home = (props) => {  
+  return (
+    <>
+      <p>
+      Hello Mundo <br />
+      {props.name} is mounted!
+      </p>
+      
+      <a href="/angular"> go to angular</a>
+    </>
+  );
+};
+
+export default Home;
